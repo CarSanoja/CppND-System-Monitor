f278bcfab2c5d897aafdb580fb712fd060abf899
Carlos <wuopcarlos@gmail.com>
2020-08-04 00:32:42 -0400

    FEAT: basic code to return usage of the cpu.
    it is calculated using the total number of jiffies compare to the idle value. More specific method could be implement further

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,18 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization()
+{
+    std::vector<std::string> jiffies_list = LinuxParser::CpuUtilization();
+    for (size_t i = 0; i < jiffies_list.size(); i++ ){
+        if ( i != LinuxParser::kGuest_ and i != LinuxParser::kGuestNice_ ){
+            total_jiffies += std::stof(jiffies_list[i]);
+        }
+        if (i==LinuxParser::kIdle_ or i==LinuxParser::kIOwait_){
+            ti_jiffies += std::stof(jiffies_list[i]);
+        }
+    }
+    usage = (total_jiffies - ti_jiffies) / total_jiffies;
+    
+    return usage; 
+}
