f693f2360f4f30b8cc01aa8083a1674e6bf92f8f
Carlos <wuopcarlos@gmail.com>
2020-08-04 00:30:49 -0400

    FEAT: adding 3 private variables to calculate cpu usage

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,20 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <iostream>
+#include "linux_parser.h"
+#include <string>
+#include <vector>
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+    float total_jiffies = 0.0;
+    float ti_jiffies = 0.0; 
+    float usage = 0.0;
 };
 
 #endif
