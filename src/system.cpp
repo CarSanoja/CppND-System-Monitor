c1f5324e93751c3c3a9255439c49ec0f0bad40ae
Carlos <wuopcarlos@gmail.com>
2020-08-04 15:10:20 -0400

    FEAT: kernel and OS assigned and compiling

--- src/system.cpp
+++ src/system.cpp
@@ -14,19 +14,34 @@ using std::string;
 using std::vector;
 
 // TODO: Return the system's CPU
-Processor& System::Cpu() { return cpu_; }
+Processor& System::Cpu()
+{
+    return cpu_;
+}
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() 
+{
+    processes_.clear();
+    std::vector<int> pids = LinuxParser::Pids();
+    
+    for (int pid : pids){
+        Process process(pid);
+        processes_.emplace_back(process);
+    }
+
+    std::sort(processes_.begin(), processes_.end(), [](Process const& a, Process const& b) {return a < b;});
+    return processes_;
+}
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
